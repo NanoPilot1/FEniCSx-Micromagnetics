5d76f07f46512f9f675186308af394e12483816d
NanoPilot1 <249293543+NanoPilot1@users.noreply.github.com>
2026-08-16 16:41:46 -0400

    Bump version to 0.2.0a1

--- src/spinfemx/__init__.py
+++ src/spinfemx/__init__.py
@@ -5,7 +5,7 @@ CPU/MPI is the default backend for standard Python installation.
 The GPU backend is experimental and intended to be used through the GPU Docker image.
 """
 
-__version__ = "0.1.0"
+__version__ = "0.2.0a1"
 
 __all__ = [
     "__version__",
